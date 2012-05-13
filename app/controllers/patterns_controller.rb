b5b82257f25b8cf56e633f80ddef09d9cc0bb9ef
Luiz Branco <artfikan@gmail.com>
2012-05-13 20:46:30 -0300

    Added Create patterns action

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -3,4 +3,17 @@ class PatternsController < ApplicationController
   def index
   end
 
+
+  def new
+  end
+
+  def create
+    @pattern = Pattern.new(params[:pattern])
+    if @pattern.save
+      redirect_to :patterns, status: :created
+    else
+      render :new, status: :bad_request
+    end
+  end
+
 end
--- spec/controllers/patterns_controller_spec.rb
+++ spec/controllers/patterns_controller_spec.rb
@@ -4,8 +4,27 @@ include ApiHelper
 describe PatternsController do
 
   it "has a list of game design patterns" do
-    get 'index'
-    response.should be_ok
+    get :index
+    response.status.should eq(200)
+  end
+
+  context "when creating a new pattern" do
+
+    it "has a new pattern page" do
+      get :new
+      response.status.should eq(200)
+    end
+
+    it "redirects to the list on success" do
+      post :create, pattern: { name: 'New Pattern', description: 'A very interesting one...' }
+      response.status.should eq(201)
+    end
+
+    it "shows the new form on error" do
+      post :create, pattern: { name: nil }
+      response.status.should eq(400)
+    end
+
   end
 
 end
